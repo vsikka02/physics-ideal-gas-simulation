76922c30e7dd24723f5c6659d550235d653cf6ed
vanshsikka <vsikka2@illinois.edu>
2021-03-23 16:37:59 -0500

    Edited Window Size and Constructor for Gas Container in Gas Simulation App.

--- src/gas_simulation_app.cc
+++ src/gas_simulation_app.cc
@@ -2,13 +2,15 @@
 
 namespace idealgas {
 
+std::vector<size_t> particle_numbers {20,10,10};
+
 IdealGasApp::IdealGasApp()
-    : container_(GasContainer(25)) {
+    : container_(GasContainer(particle_numbers)) {
   ci::app::setWindowSize(kWindowSize, kWindowSize);
 }
 
 void IdealGasApp::draw() {
-  ci::Color background_color("white");
+  ci::Color background_color("black");
   ci::gl::clear(background_color);
 
   container_.Display();
