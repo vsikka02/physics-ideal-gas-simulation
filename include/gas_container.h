dbc4b19ca92f7834262b6cf1007fef23004dc81d
vanshsikka <vsikka2@illinois.edu>
2021-03-23 16:37:07 -0500

    Altered GasContainer to be compatible with more than 1 particle type.

--- include/gas_container.h
+++ include/gas_container.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "cinder/gl/gl.h"
+#include "histogram.h"
 #include "particle.h"
 
 using glm::vec2;
@@ -14,20 +15,11 @@ namespace idealgas {
 class GasContainer {
  public:
   /**
-   * These 2 constants are used to generate the size of the rectangle that
-   * represents the container for all of the gas particle. kOutOfBounds1
-   * represents the top-left corner of the gas container. and the kOutOfBounds2
-   * represents the lower-right corner of the rectangular gas container.
-   */
-  const vec2 kOutOfBounds1 = vec2(100, 100);
-  const vec2 kOutOfBounds2 = vec2(650, 650);
-
-  /**
    * This is the constructor for the container. This takes in the amount of
    * gas-particles that you want inside the simulation and will place all of
    * these particles at random locations with random velocities.
    */
-  GasContainer(int number_particles);
+  GasContainer(std::vector<size_t> num_different_particles);
 
   /**
    * Displays the container walls and the current positions of the particles.
@@ -39,7 +31,11 @@ class GasContainer {
    * described in the assignment documentation).
    */
   void AdvanceOneFrame();
-  std::vector<idealgas::Particle> GetParticleVector();
+  std::vector<idealgas::Particle> GetAllParticles();
+
+  void CreateSizeVectors();
+
+  void CreateHistograms();
 
  private:
   /**
@@ -47,7 +43,29 @@ class GasContainer {
    * for collisions with walls and each other. This stores all the particles that
    * are inside the gas chamber.
    */
-  std::vector<idealgas::Particle> particle_vector_;
+  std::vector<Particle> all_particles_;
+  std::vector<Particle> small_particles_;
+  std::vector<Particle> medium_particles_;
+  std::vector<Particle> large_particles_;
+  std::vector<Histogram> histograms_;
+
+  /**
+ * These 2 constants are used to generate the size of the rectangle that
+ * represents the container for all of the gas particle. kOutOfBounds1
+ * represents the top-left corner of the gas container. and the kOutOfBounds2
+ * represents the lower-right corner of the rectangular gas container.
+ */
+  const vec2 kOutOfBounds1 = vec2(100, 25);
+  const vec2 kOutOfBounds2 = vec2(700, 425);
+  const int kLargeParticleRadius = 10;
+  const int kMediumParticleRadius = 7;
+  const int kSmallParticleRadius = 5;
+  const ci::Color kLargeParticleColor = ci::Color("green");
+  const ci::Color kMediumParticleColor = ci::Color("blue");
+  const ci::Color kSmallParticleColor = ci::Color("pink");
+  const float kLargeParticleMass = 100;
+  const float kMediumParticleMass = 25;
+  const float kSmallParticleMass = 5;
 };
 
-}  // namespace idealgas
+};  // namespace idealgas
--- src/gas_container.cc
+++ src/gas_container.cc
@@ -1,71 +1,112 @@
 #include "gas_container.h"
 
-#include <iostream>
 
 namespace idealgas {
 
 using glm::vec2;
 using std::vector;
 
-GasContainer::GasContainer(int number_particle) {
-  if (number_particle < 0) {
-    throw std::invalid_argument("Cannot have negative particles");
+GasContainer::GasContainer(std::vector<size_t> num_different_particles) {
+  if (num_different_particles.size() > 3) {
+    throw std::invalid_argument("Cannot have more than 3 different particles!");
   }
+
   std::srand(std::time(nullptr));
-  for (int i = 0; i < number_particle; i++) {
-    int rad = 10;
 
-    int x_position =
-        rand() % static_cast<int>(kOutOfBounds2.x - kOutOfBounds1.x - rad) +
-        kOutOfBounds1.x + rad;
+  for (size_t i = 0; i <  num_different_particles.size(); i++) {
+    for (size_t j = 0; j < num_different_particles[i]; j++) {
+      int x_position = rand() % static_cast<size_t>(kOutOfBounds2.x -
+                                                 kOutOfBounds1.x - 2 * kLargeParticleRadius) +
+                       kOutOfBounds1.x + kLargeParticleRadius;
 
-    int y_position =
-        rand() % static_cast<int>(kOutOfBounds2.y - kOutOfBounds1.y - rad) +
-        kOutOfBounds1.y + rad;
+      int y_position = rand() % static_cast<size_t>(kOutOfBounds2.y -
+                                                 kOutOfBounds1.y - 2 * kLargeParticleRadius) +
+                       kOutOfBounds1.y + kLargeParticleRadius;
 
-    float x_velocity =
-        (static_cast<float>(rand() % 100) / static_cast<float>(25)) - 2.0;
+      float x_velocity =
+          (static_cast<float>(rand() % 100) / static_cast<float>(25)) - 2.0;
 
-    float y_velocity =
-        (static_cast<float>(rand() % 100) / static_cast<float>(25)) - 2.0;
+      float y_velocity =
+          (static_cast<float>(rand() % 100) / static_cast<float>(25)) - 2.0;
 
-    particle_vector_.emplace_back(vec2(x_position, y_position),
-                                  vec2(x_velocity, y_velocity),
-                                  ci::Color("green"), 0, rad);
+      if (i == 0) {
+        Particle particle = Particle(vec2(x_position, y_position), vec2(x_velocity, y_velocity), kSmallParticleColor, kSmallParticleMass, kSmallParticleRadius);
+        all_particles_.push_back(particle);
+      } else if (i == 1) {
+        Particle particle = Particle(vec2(x_position, y_position), vec2(x_velocity, y_velocity), kMediumParticleColor, kMediumParticleMass, kMediumParticleRadius);
+        all_particles_.push_back(particle);
+      } else if (i == 2) {
+        Particle particle = Particle(vec2(x_position, y_position), vec2(x_velocity, y_velocity), kLargeParticleColor, kLargeParticleMass, kLargeParticleRadius);
+        all_particles_.push_back(particle);
+      }
+    }
   }
 }
 
 void GasContainer::Display() const {
   // This function has a lot of magic numbers; be sure to design your code in a
   // way that avoids this.
-  ci::gl::color(ci::Color("black"));
+  ci::gl::color(ci::Color("white"));
   ci::gl::drawStrokedRect(ci::Rectf(kOutOfBounds1, kOutOfBounds2));
 
-  for (Particle particle : particle_vector_) {
+  for (Particle particle : all_particles_) {
     ci::gl::color(particle.GetColor());
     ci::gl::drawSolidCircle(vec2(particle.GetPosition()), particle.GetRadius());
+  }
+
+  for (Histogram histogram: histograms_) {
+    histogram.DrawHistogram();
   }
 }
 
 void GasContainer::AdvanceOneFrame() {
-
-  for (Particle& particle : particle_vector_) {
+  for (Particle& particle : all_particles_) {
     particle.UpdateVelocityAfterWallCollision(kOutOfBounds1, kOutOfBounds2);
     particle.UpdatePosition();
   }
 
-  for (int i = 0; i < particle_vector_.size() - 1; i++) {
-    for (int j = i + 1; j < particle_vector_.size(); j++) {
-
-      if (particle_vector_[i].IsColliding(particle_vector_[j])) {
-        particle_vector_[i].SetCollidedVelocity(particle_vector_[j]);
+  for (size_t i = 0; i < all_particles_.size() - 1; i++) {
+    for (size_t j = i + 1; j < all_particles_.size(); j++) {
+      if (all_particles_[i].IsColliding(all_particles_[j])) {
+        all_particles_[i].SetCollidedVelocity(all_particles_[j]);
       }
+    }
+  }
 
+  CreateSizeVectors();
+  CreateHistograms();
+
+  for (Histogram& histogram : histograms_) {
+    histogram.UpdateHistogram();
+  }
+}
+
+std::vector<Particle> GasContainer::GetAllParticles() {
+  return all_particles_;
+}
+
+void GasContainer::CreateSizeVectors() {
+  large_particles_.clear();
+  medium_particles_.clear();
+  small_particles_.clear();
+
+  for (Particle particle: all_particles_) {
+    if (particle.GetRadius() == kLargeParticleRadius) {
+      large_particles_.push_back(particle);
+    } else if (particle.GetRadius() == kMediumParticleRadius) {
+      medium_particles_.push_back(particle);
+    } else if (particle.GetRadius() == kSmallParticleRadius) {
+      small_particles_.push_back(particle);
     }
   }
 }
 
-std::vector<Particle> GasContainer::GetParticleVector() {
-  return particle_vector_;
+void GasContainer::CreateHistograms() {
+  histograms_.clear();
+
+  histograms_.push_back(Histogram(small_particles_, vec2(45, 500), vec2(245, 700), kSmallParticleColor, "Small Particles Histogram"));
+  histograms_.push_back(Histogram(medium_particles_, vec2(295, 500), vec2(495, 700), kMediumParticleColor, "Medium Particles Histogram"));
+  histograms_.push_back(Histogram(large_particles_, vec2(545, 500), vec2(745, 700), kLargeParticleColor, "Large Particles Histogram"));
 }
+
 }  // namespace idealgas
